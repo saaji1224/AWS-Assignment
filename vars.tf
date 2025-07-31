4ed1ce20c28a16e60076bc078ce5f9649181d249
Shaik Gouse Mohiddin Saida Sajith <110539389+saaji1224@users.noreply.github.com>
2025-07-31 20:24:03 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  description = "AWS region for the VPC"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+variable "firewall_subnet_cidrs" {
+  description = "CIDR blocks for firewall subnets"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
+}
